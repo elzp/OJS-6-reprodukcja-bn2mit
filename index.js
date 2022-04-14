a6a8c5ecb4a7fcceb55dcd5fc6d1627fc08534aa
elzp <piechelzbieta@wp.pl>
2022-04-14 21:22:32 +0200

    Task compleated

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ import './style.css';
 const PIXEL_COLOR = '#e44d4d';
 
 const fakeCanvas = document.querySelector('.fake-canvas');
+const originCanvas = document.querySelector('.original-canvas');
+const fakeDivs = fakeCanvas.querySelectorAll('.flex-row > div');
+const originDivs = originCanvas.querySelectorAll('.flex-row > div');
+
+originDivs.forEach((item, index) => {
+  if (item.className.split(' ').includes('pixel-dot')) {
+    fakeDivs[index].classList.add('pixel-dot');
+  }
+});
